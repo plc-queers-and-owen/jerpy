8cdf36f69b920f85fe5b98d770260706f8cebdb0
Dax Harris <me@dax.gay>
2024-09-06 10:11:10 -0400

    file read setup

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -1,5 +1,8 @@
 package provided;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * This class is responsible for tokenizing Jott code.
  * 
@@ -9,16 +12,41 @@ package provided;
 import java.util.ArrayList;
 
 public class JottTokenizer {
+    public static ArrayList<Token> tokenize_line(String line) {
+        ArrayList<Token> tokens = new ArrayList<>();
+        System.out.println(line);
 
-  /**
-   * Takes in a filename and tokenizes that file into Tokens
-   * based on the rules of the Jott Language
-   * 
-   * @param filename the name of the file to tokenize; can be relative or absolute
-   *                 path
-   * @return an ArrayList of Jott Tokens
-   */
-  public static ArrayList<Token> tokenize(String filename) {
-    return null;
-  }
+        return tokens;
+    }
+
+    /**
+     * Takes in a filename and tokenizes that file into Tokens
+     * based on the rules of the Jott Language
+     * 
+     * @param filename the name of the file to tokenize; can be relative or absolute
+     *                 path
+     * @return an ArrayList of Jott Tokens
+     */
+    public static ArrayList<Token> tokenize(String filename) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            ArrayList<Token> tokens = new ArrayList<Token>();
+            while (true) {
+                try {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    tokens.addAll(JottTokenizer.tokenize_line(line));
+                } catch (Exception e) {
+                    break;
+                }
+            }
+            reader.close();
+
+            return tokens;
+        } catch (Exception e) {
+            return new ArrayList<Token>();
+        }
+    }
 }
