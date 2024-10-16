db67c043f4a530c32d00c304dc97a4f138340d62
Avery Carle <ajc4018@rit.edu>
2024-10-15 22:07:01 -0400

    create ParamsNode

---
+++ src/internal/nodes/ParamsNode.java
@@ -0,0 +1,28 @@
+package internal.nodes;
+
+public class ParamsNode extends Node{
+
+    protected ParamsNode(int lineNumber) {
+        super(lineNumber);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
