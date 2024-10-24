d936600cf4a3893a5d04d8e7870218ecf9f28d3c
Dax Harris <me@dax.gay>
2024-10-24 15:34:49 -0400

    more stuff for main Jott class

--- src/Jott.java
+++ src/Jott.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+
+import provided.JottParser;
 import provided.JottTokenizer;
+import provided.JottTree;
+import provided.Token;
 
 public class Jott {
 
     public static void main(String[] args) {
-        JottTokenizer.tokenize(args[0]);
+        ArrayList<Token> tokens = JottTokenizer.tokenize(args[0]);
+        JottTree parsedTree = JottParser.parse(tokens);
+        if (parsedTree == null) {
+            return;
+        } else {
+            {
+                System.out.println(parsedTree.convertToJott());
+            }
+        }
+
     }
 
 }
