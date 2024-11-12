59f7d3b75c8e97704b691258f2fd729cde1345f2
Dax Harris <me@dax.gay>
2024-11-12 15:43:47 -0500

    setup for testing ph3

--- src/Jott.java
+++ src/Jott.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
+import internal.nodes.ProgramNode;
+import provided.JottParser;
 import provided.JottTokenizer;
+import provided.Token;
 
 public class Jott {
 
     public static void main(String[] args) {
-        JottTokenizer.tokenize(args[0]);
+        ArrayList<Token> tokens = JottTokenizer.tokenize(args[0]);
+        ProgramNode program = (ProgramNode) JottParser.parse(tokens);
+        if (program != null) {
+            System.out.println(program.convertToJott());
+        }
     }
-
 }
