66bf075abea60f744cf9880d2551d8efb77bc13a
Dax Harris <me@dax.gay>
2024-11-14 17:19:09 -0500

    remove jot t printing

--- src/Jott.java
+++ src/Jott.java
@@ -10,8 +10,5 @@ public class Jott {
     public static void main(String[] args) {
         ArrayList<Token> tokens = JottTokenizer.tokenize(args[0]);
         ProgramNode program = (ProgramNode) JottParser.parse(tokens);
-        if (program != null) {
-            System.out.println(program.convertToJott());
-        }
     }
 }
